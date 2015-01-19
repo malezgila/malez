c545f9bdddd37f521d1315a1aba5897ce9d21b01
ricky.cool46@ymail.com <ricky.cool46@ymail.com@2d07e57a-0850-56e8-50b1-d62afac6a481>
2015-01-19 13:42:43 +0000

    1

--- emotrev1.js
+++ emotrev1.js
@@ -44,6 +44,27 @@ function addSmiley(){
 			d[i].innerHTML = d[i].innerHTML.replace(/!tonjok/gi, "<img src='http://1.bp.blogspot.com/-JnTQzIIkKXk/VLUYPLBgi5I/AAAAAAAAAPo/vuvrjyVqTes/s1600/tonjok.gif' alt='' class='bhsmly'/>");
 			d[i].innerHTML = d[i].innerHTML.replace(/!top/gi, "<img src='http://1.bp.blogspot.com/-gBJQQnC6Ru0/VLUYPYR-7FI/AAAAAAAAAPs/K-wTlBEr650/s1600/top.gif' alt='' class='bhsmly'/>");
 			d[i].innerHTML = d[i].innerHTML.replace(/!mangap/gi, "<img src='http://4.bp.blogspot.com/-gWht_GMbX6Q/VLUYQ21xLKI/AAAAAAAAAPw/8e54ALAb-LA/s1600/v.png' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!happy/gi, "<img src='http://4.bp.blogspot.com/-azwhAsB7WW8/VL0Cw2s0rYI/AAAAAAAAAR0/drNEUbP27V0/s1600/aboishappyplz.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!boong/gi, "<img src='http://1.bp.blogspot.com/-0DJ6CZF1ArE/VL0EGDGigTI/AAAAAAAAASE/H08ZeP8rSEQ/s1600/boong.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!capedeh/gi, "<img src='http://4.bp.blogspot.com/-gNOjLH5jqQg/VL0EGHkAyrI/AAAAAAAAASA/AXguhgyCdgc/s1600/capedeh.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!dead/gi, "<img src='http://2.bp.blogspot.com/-OfE20E3o7Lo/VL0EGGMrDjI/AAAAAAAAAS0/2WX-seGb59o/s1600/dead.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!joget/gi, "<img src='http://4.bp.blogspot.com/-lGwjGqt64wM/VL0EIST8zEI/AAAAAAAAASU/q74UUV1F-tg/s1600/joget2.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!galau/gi, "<img src='http://4.bp.blogspot.com/-EIVikhhoPdM/VL0EHmtN0TI/AAAAAAAAASM/CDXERw_wSOg/s1600/galau.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!lega/gi, "<img src='http://1.bp.blogspot.com/-6-aD7gkIBkQ/VL0EJ4s10vI/AAAAAAAAASk/mpGvSVBi4iw/s1600/lega.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!maaf/gi, "<img src='http://4.bp.blogspot.com/-mSjusoGULic/VL0EKxIF7PI/AAAAAAAAASw/HWnILv5CHNw/s1600/maaf.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!matabelo/gi, "<img src='http://4.bp.blogspot.com/-vtIUK6YrynQ/VL0ELtc6z5I/AAAAAAAAATA/vepFy9y8qTw/s1600/matabelo.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!nangis/gi, "<img src='http://1.bp.blogspot.com/-l7raEsyGBFk/VL0EMjylj6I/AAAAAAAAATE/bp29vmNv0x8/s1600/nangis.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!ngece/gi, "<img src='http://2.bp.blogspot.com/-Axl7mf3ibNU/VL0ENAbPnEI/AAAAAAAAAT8/d4aW4g4Z7Xc/s1600/ngece.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!prustasi/gi, "<img src='http://3.bp.blogspot.com/-1e0VFVRPa30/VL0EOPqUEQI/AAAAAAAAATU/fmPr4lPblVA/s1600/prustasi.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!relax/gi, "<img src='http://1.bp.blogspot.com/-s1MLzwDM148/VL0EPdrbo4I/AAAAAAAAATg/kIh6hHioySI/s1600/relax.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!minta/gi, "<img src='http://2.bp.blogspot.com/-FWbpIuTJCOc/VL0EP_uDS-I/AAAAAAAAATk/FfDBEqenM-A/s1600/tagih.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!terharu/gi, "<img src='http://3.bp.blogspot.com/-RIWSJjDh03k/VL0ERexIBXI/AAAAAAAAATo/6AyoSJhNyiw/s1600/terharu.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!terkejut/gi, "<img src='http://4.bp.blogspot.com/-B1aO-Y2G1ns/VL0ESCSh-KI/AAAAAAAAATs/mSsQY3kVP7A/s1600/terkejut.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!kaget/gi, "<img src='http://2.bp.blogspot.com/-95jJqoRMJU8/VL0EJvWawUI/AAAAAAAAASc/TeXQtp4IV2E/s1600/kaget.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!pasrah/gi, "<img src='http://1.bp.blogspot.com/-yQx3TPfpFos/VL0ENiQT0aI/AAAAAAAAATQ/1KCEESjK20w/s1600/pasrah.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!sepi/gi, "<img src='http://1.bp.blogspot.com/-yQx3TPfpFos/VL0ENiQT0aI/AAAAAAAAATQ/1KCEESjK20w/s1600/pasrah.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!yes/gi, "<img src='http://4.bp.blogspot.com/-djRpXJkvv8Y/VL0EURPLuiI/AAAAAAAAAUI/LJJnkeWEOHk/s1600/yes.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!wawa/gi, "<img src='http://2.bp.blogspot.com/-4-GpDgxvkqQ/VL0ET6iPS-I/AAAAAAAAAUE/b17pHS6iFvw/s1600/wawa.gif' alt='' class='bhsmly'/>");
 			d[i].innerHTML = d[i].innerHTML.replace(/!wow/gi, "<img src='http://4.bp.blogspot.com/-qpsSc-VkU8k/VLUYRjUyXNI/AAAAAAAAAQA/yqAEppZbpco/s1600/wow.gif' alt='' class='bhsmly'/>")
 		
     }
@@ -72,6 +93,27 @@ var d = document.getElementsByClassName('widget-content');
 			d[i].innerHTML = d[i].innerHTML.replace(/!tonjok/gi, "<img src='http://1.bp.blogspot.com/-JnTQzIIkKXk/VLUYPLBgi5I/AAAAAAAAAPo/vuvrjyVqTes/s1600/tonjok.gif' alt='' class='bhsmly'/>");
 			d[i].innerHTML = d[i].innerHTML.replace(/!top/gi, "<img src='http://1.bp.blogspot.com/-gBJQQnC6Ru0/VLUYPYR-7FI/AAAAAAAAAPs/K-wTlBEr650/s1600/top.gif' alt='' class='bhsmly'/>");
 			d[i].innerHTML = d[i].innerHTML.replace(/!mangap/gi, "<img src='http://4.bp.blogspot.com/-gWht_GMbX6Q/VLUYQ21xLKI/AAAAAAAAAPw/8e54ALAb-LA/s1600/v.png' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!happy/gi, "<img src='http://4.bp.blogspot.com/-azwhAsB7WW8/VL0Cw2s0rYI/AAAAAAAAAR0/drNEUbP27V0/s1600/aboishappyplz.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!boong/gi, "<img src='http://1.bp.blogspot.com/-0DJ6CZF1ArE/VL0EGDGigTI/AAAAAAAAASE/H08ZeP8rSEQ/s1600/boong.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!capedeh/gi, "<img src='http://4.bp.blogspot.com/-gNOjLH5jqQg/VL0EGHkAyrI/AAAAAAAAASA/AXguhgyCdgc/s1600/capedeh.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!dead/gi, "<img src='http://2.bp.blogspot.com/-OfE20E3o7Lo/VL0EGGMrDjI/AAAAAAAAAS0/2WX-seGb59o/s1600/dead.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!joget/gi, "<img src='http://4.bp.blogspot.com/-lGwjGqt64wM/VL0EIST8zEI/AAAAAAAAASU/q74UUV1F-tg/s1600/joget2.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!galau/gi, "<img src='http://4.bp.blogspot.com/-EIVikhhoPdM/VL0EHmtN0TI/AAAAAAAAASM/CDXERw_wSOg/s1600/galau.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!lega/gi, "<img src='http://1.bp.blogspot.com/-6-aD7gkIBkQ/VL0EJ4s10vI/AAAAAAAAASk/mpGvSVBi4iw/s1600/lega.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!maaf/gi, "<img src='http://4.bp.blogspot.com/-mSjusoGULic/VL0EKxIF7PI/AAAAAAAAASw/HWnILv5CHNw/s1600/maaf.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!matabelo/gi, "<img src='http://4.bp.blogspot.com/-vtIUK6YrynQ/VL0ELtc6z5I/AAAAAAAAATA/vepFy9y8qTw/s1600/matabelo.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!nangis/gi, "<img src='http://1.bp.blogspot.com/-l7raEsyGBFk/VL0EMjylj6I/AAAAAAAAATE/bp29vmNv0x8/s1600/nangis.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!ngece/gi, "<img src='http://2.bp.blogspot.com/-Axl7mf3ibNU/VL0ENAbPnEI/AAAAAAAAAT8/d4aW4g4Z7Xc/s1600/ngece.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!prustasi/gi, "<img src='http://3.bp.blogspot.com/-1e0VFVRPa30/VL0EOPqUEQI/AAAAAAAAATU/fmPr4lPblVA/s1600/prustasi.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!relax/gi, "<img src='http://1.bp.blogspot.com/-s1MLzwDM148/VL0EPdrbo4I/AAAAAAAAATg/kIh6hHioySI/s1600/relax.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!minta/gi, "<img src='http://2.bp.blogspot.com/-FWbpIuTJCOc/VL0EP_uDS-I/AAAAAAAAATk/FfDBEqenM-A/s1600/tagih.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!terharu/gi, "<img src='http://3.bp.blogspot.com/-RIWSJjDh03k/VL0ERexIBXI/AAAAAAAAATo/6AyoSJhNyiw/s1600/terharu.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!terkejut/gi, "<img src='http://4.bp.blogspot.com/-B1aO-Y2G1ns/VL0ESCSh-KI/AAAAAAAAATs/mSsQY3kVP7A/s1600/terkejut.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!kaget/gi, "<img src='http://2.bp.blogspot.com/-95jJqoRMJU8/VL0EJvWawUI/AAAAAAAAASc/TeXQtp4IV2E/s1600/kaget.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!pasrah/gi, "<img src='http://1.bp.blogspot.com/-yQx3TPfpFos/VL0ENiQT0aI/AAAAAAAAATQ/1KCEESjK20w/s1600/pasrah.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!sepi/gi, "<img src='http://1.bp.blogspot.com/-yQx3TPfpFos/VL0ENiQT0aI/AAAAAAAAATQ/1KCEESjK20w/s1600/pasrah.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!yes/gi, "<img src='http://4.bp.blogspot.com/-djRpXJkvv8Y/VL0EURPLuiI/AAAAAAAAAUI/LJJnkeWEOHk/s1600/yes.gif' alt='' class='bhsmly'/>");
+      d[i].innerHTML = d[i].innerHTML.replace(/!wawa/gi, "<img src='http://2.bp.blogspot.com/-4-GpDgxvkqQ/VL0ET6iPS-I/AAAAAAAAAUE/b17pHS6iFvw/s1600/wawa.gif' alt='' class='bhsmly'/>");
 			d[i].innerHTML = d[i].innerHTML.replace(/!wow/gi, "<img src='http://4.bp.blogspot.com/-qpsSc-VkU8k/VLUYRjUyXNI/AAAAAAAAAQA/yqAEppZbpco/s1600/wow.gif' alt='' class='bhsmly'/>")
 
 }
